5e8894f8487cf5543de3026a2b21acebfe2a1eea
affif443 <80408741+affif443@users.noreply.github.com>
2021-05-22 21:10:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const constraint=Matter.constraint;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    roof= new Roof(425,300,250,20);
+	bobObject1= new Bob(340,470,20);
+	bobObject2= new Bob(380,470,20);
+	bobObject3= new Bob(420,470,20);
+	bobObject4= new Bob(460,470,20);
+    bobObject5= new Bob(500,470,20);
+
+	rope1=new Rope(bobObject1.body,roof.body,-40*2,0);
+	rope2=new Rope(bobObject2.body,roof.body,-20*2,0);
+	rope3=new Rope(bobObject3.body,roof.body,0*2,0);
+	rope4=new Rope(bobObject4.body,roof.body,20*2,0);
+	rope5=new Rope(bobObject5.body,roof.body,40*2,0);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+  roof.display();
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+}
+
+function drawline(constraint){
+	bobBodyPosition=constraint.bodyA.position;
+	roofBodyPosition=constraint.bodyB.position;
+	roofbodyOffset=constraint.PointB;
+	roofbodyX=roofBodyPosition.x+roofbodyOffset.x;
+	roofbodyY=roofBodyPosition.y+roofbodyOffset.y;
+	Line(bobBodyPosition.x,bobBodyPosition.y,roofbodyX,roofbodyY);
+
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyforce(bobObject1.body,bobObject1.body.position ,{x:-58,y:-45});
+	}
+}
